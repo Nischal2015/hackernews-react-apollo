ad9287c2b97d73ee2c7bf5570458779d2756759f
Nischal Shakya <nischalshakya2015@gmail.com>
2022-11-20 20:51:58 +0545

    chore: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>
+        This is the graphql tutorial using <strong>Apollo Client</strong> and{' '}
+        <strong>React</strong>.
+      </p>
+    </div>
+  );
+}
+
+export default App;
